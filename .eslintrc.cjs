f08ab6d10daff0593b518176424885cf33a9f033
Ex10 <ca02020@cartahd.com>
2024-06-09 18:48:45 +0900

    適用 ref: https://github.com/ikoan08/GEATS/pull/5

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,23 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    parser: '@typescript-eslint/parser',
-    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-    settings: { react: { version: '18.2' } },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:react-hooks/recommended',
-        'plugin:@typescript-eslint/recommended',
+  root: true,
+  env: { browser: true, es2020: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  plugins: ["react-refresh", "@typescript-eslint"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    plugins: [
-        'react-refresh',
-        '@typescript-eslint',
-    ],
-    rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
-    },
-}
+  },
+};
