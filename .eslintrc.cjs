ae672fb3cc4e8722e66fd6f55d7e43004a8b54fd
Ex10 <ca02020@cartahd.com>
2024-06-09 18:26:32 +0900

    設定を書いて通した ref: https://github.com/ikoan08/GEATS/pull/4

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    parser: '@typescript-eslint/parser',
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    plugins: [
+        'react-refresh',
+        '@typescript-eslint',
+    ],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+    },
+}
